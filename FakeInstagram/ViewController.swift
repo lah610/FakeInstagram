a7b411c5a906804ba5dabc00016265855a946981
Luis Arenas <u1@MacBook-Pro-de-LUIS-2.local>
2019-03-30 11:57:26 -0600

    me lo dijo martin

---
+++ FakeInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeInstagram
+//
+//  Created by U1 on 30/03/19.
+//  Copyright © 2019 Luis Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
